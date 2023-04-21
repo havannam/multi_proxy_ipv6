2ebdf4defc98e32a459897036068e8439465b12b
namhv <namclls@gmail.com>
2023-04-21 19:34:18 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -108,23 +108,17 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 FIRST_PORT=40000
 LAST_PORT=42000
 
-gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 echo NM_CONTROLLED="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
-chmod +x $WORKDIR/boot_*.sh /etc/rc.local
+chmod +x $WORKDIR/v62.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
 #systemctl start NetworkManager.service
 # ifup ${main_interface}
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
+bash ${WORKDIR}/v62.sh
 ulimit -n 65535
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
 EOF
-
-bash /etc/rc.local
 
 gen_proxy_file_for_user
